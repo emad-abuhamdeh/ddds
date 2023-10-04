5ea5682d969fa900361a08a69115370aca70283b
emad-abuhamdeh <112353622+emad-abuhamdeh@users.noreply.github.com>
2023-10-04 11:12:18 -0700

    Initial commit

---
+++ components/HeroSection.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  import { pg_background_urls } from '~~/themes/pg-tailwindcss/tokens.mjs'
+
+  const heroImageUrl =
+    pg_background_urls['design-image-large'] ||
+    pg_background_urls['design-image']
+
+  const { optimizeImage } = useOptimizeImage()
+  const { bgStyles } = optimizeImage(heroImageUrl)
+</script>
+<template>
+  <section>
+    <div
+      class="bg-center bg-cover bg-no-repeat blur-none z-0 bg-design-image lg:bg-design-image-large"
+    >
+      <div
+        class="pb-36 pt-2 px-6 relative rounded-3xl md:pb-48 lg:pb-72 lg:px-12"
+      >
+        <div
+          class="-mx-4 flex flex-wrap items-center justify-center lg:space-y-0 mt-24 space-y-6"
+        >
+          <div class="px-4 text-center w-full md:w-8/12 xl:w-6/12">
+            <h4
+              class="font-bold font-serif mb-1 text-secondary-600 dark:text-secondary-200 uppercase"
+            >
+              Welcome to
+            </h4>
+            <h1 class="capitalize leading-tight mb-2 text-white">
+              Vue Designer
+            </h1>
+            <h5 class="mb-6 text-gray-300">
+              A desktop drag-and-drop editor for Mac, Windows and Linux that
+              lets you visually design your Vue apps.
+            </h5>
+            <BaseButton
+              class="!rounded-3xl px-6 py-4"
+              to="https://vuedesigner.com"
+              trailing
+              target="_blank"
+              trailing-icon="i-material-symbols-open-in-new"
+              size="xl"
+              label="Learn More!"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,156 @@
+// import { fileURLToPath, URL } from 'node:url'
+import presetIcons from '@unocss/preset-icons'
+
+export default defineNuxtConfig({
+  // ssr: false,
+  devtools: { enabled: false }, // Disable when using Vue devtools
+
+  // Look into MetaTags.vue for other flavours
+  app: {
+    baseURL: '/',
+    head: {
+      meta: [{ charset: 'utf-8' }],
+    },
+  },
+
+  modules: [
+    '@pinegrow/nuxt-module',
+    '@unocss/nuxt',
+    '@nuxt/devtools',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    // '@nuxtjs/html-validator',
+    '@nuxt/image',
+    '@vee-validate/nuxt',
+  ],
+
+  pinegrow: {
+    liveDesigner: {
+      iconPreferredCase: 'unocss', // default value (can be removed), unocss by default uses the unocss format for icon names names
+      devtoolsKey: 'devtools', // see plugins/devtools.client.ts
+      tailwindcss: {
+        configPath: 'tailwind.config.ts',
+        cssPath: '@/assets/css/tailwind.css',
+        // themePath: false, // Set to false so that Design Panel is not used
+        // restartOnConfigUpdate: true,
+        restartOnThemeUpdate: true,
+      },
+      // plugins: [
+      //   {
+      //     name: 'My Awesome Lib 3.0',
+      //     key: 'my-awesome-lib',
+      //     pluginPath: fileURLToPath(
+      //       new URL('./my-awesome-lib/web-types.json', import.meta.url),
+      //     ),
+      //   },
+      // ],
+    },
+  },
+
+  css: [
+    '~/assets/css/tailwind.css',
+    'lite-youtube-embed/src/lite-yt-embed.css',
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  image: {
+    // sizes: 'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw', // Not yet supported - https://github.com/nuxt/image/issues/216
+    // densities: [1,2], // default
+    quality: 80, // can be overridden as NuxtImg prop
+    format: ['webp'], // default
+    // The screen sizes predefined by `@nuxt/image`:
+    // screens: {
+    //   xs: 320,
+    //   sm: 640,
+    //   md: 768,
+    //   lg: 1024,
+    //   xl: 1280,
+    //   xxl: 1536,
+    //   '2xl': 1536,
+    // },
+    presets: {
+      avatar: {
+        modifiers: {
+          format: 'webp',
+          width: 80,
+          height: 80,
+        },
+      },
+    },
+    domains: ['images.unsplash.com', 'fakestoreapi.com', 'res.cloudinary.com'],
+    alias: {
+      unsplash: 'https://images.unsplash.com',
+    },
+  },
+
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray',
+      ErrorMessage: 'VeeErrorMessage',
+    },
+  },
+
+  content: {
+    markdown: {
+      anchorLinks: false,
+      rehypePlugins: [
+        [
+          'rehype-external-links',
+          {
+            target: '_blank',
+            rel: ['noopener'],
+            test: (node: any) => /^https?:\/\//.test(node.properties.href),
+          },
+        ],
+      ],
+    },
+    highlight: {
+      theme: 'dracula-soft',
+    },
+  },
+
+  unocss: {
+    presets: [
+      presetIcons({
+        prefix: 'i-', // default prefix, do not change
+      }),
+    ],
+  },
+
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+      'storeToRefs',
+      'acceptHMRUpdate',
+    ],
+  },
+
+  imports: {
+    dirs: ['stores'],
+  },
+
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag === 'lite-youtube',
+    },
+  },
+
+  sourcemap: {
+    client: false,
+    server: false,
+  },
+})
